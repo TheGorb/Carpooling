0476e810f36a653071551d610d6143def3790c20
GuiRch <guillaumericher@outlook.fr>
2022-07-12 10:36:39 +0200

    first pages

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import type {Node} from 'react';
 
 // Pages
 import Home from './src/components/pages/Home';
+import Profile from './src/components/pages/Profile';
+import PublishJourney from './src/components/pages/PublishJourney';
 
 // Utils 
 import {NavigationContainer} from '@react-navigation/native';
@@ -21,6 +23,8 @@ const App = () => {
             headerShown: false,
           }}>
           <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="PublishJourney" component={PublishJourney} />
         </Stack.Navigator>
       </NavigationContainer>
   );
